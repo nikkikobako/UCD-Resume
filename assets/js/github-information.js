8774988cb06cc4434a722615acbdddb0a49a9320
Nicolette Shaw <nikkikobako@gmail.com>
2020-11-01 10:46:32 +0000

    fetch repo info and display

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -50,7 +50,7 @@ function fetchGitHubInformation(event) {
         </div>`);
 
     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`),
         $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
          function(firstResponse, secondResponse) {
